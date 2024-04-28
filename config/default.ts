91ca3b2d72cd6d6a06c6c7eb4acd3d8df5fe3ec8
kuznecoviv <kuzvanya94@gmail.com>
2024-04-28 23:23:26 +0300

    :package: init db

---
+++ config/default.ts
@@ -0,0 +1,16 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export default {
+  server: {
+    port: '3000',
+  },
+  typeORMDataSource: {
+    type: 'postgres',
+    host: '127.0.0.1',
+    port: 5432,
+    username: 'postgres',
+    password: '',
+    database: 'pallete_system',
+    namingStrategy: new SnakeNamingStrategy(),
+  },
+};
---
+++ typeORM.dataSource.ts
@@ -0,0 +1,7 @@
+import { DataSource } from 'typeorm';
+import * as config from 'config';
+
+export default new DataSource({
+  ...config.get('typeORMDataSource'),
+  migrations: ['dist/src/migrations/*.js'],
+});
